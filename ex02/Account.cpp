86397df9a515b9f23d5135c3919984684f923aba
Houdaifa Drahm <hdrahm@c3r5p10.1337.ma>
2025-03-13 20:47:17 +0000

    done

---
+++ ex02/Account.cpp
@@ -0,0 +1,142 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdrahm <hdrahm@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 11:33:49 by hdrahm            #+#    #+#             */
+/*   Updated: 2025/03/13 20:39:40 by hdrahm           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+# include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+
+void Account::_displayTimestamp(void)
+{
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "[%Y%m%d_%H%M%S] ", &tstruct);
+    std::cout << buf ;
+    
+
+}
+
+
+Account::Account(int initial_deposit) : _accountIndex (_nbAccounts),
+    _amount(initial_deposit), _nbDeposits(0), _nbWithdrawals(0) 
+{
+    _nbAccounts++;
+    _totalAmount += initial_deposit;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:"<<_amount
+              << ";created" << std::endl; 
+}
+
+
+int Account::getNbAccounts( void )
+{
+    return (_nbAccounts);
+}
+
+int Account::getTotalAmount( void )
+{
+    return (_totalAmount);
+}
+
+int Account::getNbDeposits( void )
+{
+    return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals ( void )
+{
+    return (_totalNbWithdrawals);
+}
+
+void Account::displayAccountsInfos ( void )
+{
+    _displayTimestamp();
+    std::cout << "accounts:" << _nbAccounts
+              << ";total:"<< _totalAmount
+              << ";deposits:" << _totalNbDeposits
+              << ";withdrawals:"<< _totalNbWithdrawals
+              << std::endl;    
+}
+
+void Account::makeDeposit( int deposit )
+{
+    int new_amount = _amount + deposit;
+    
+    _nbDeposits++;
+    _totalNbDeposits++;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";p_amount:"<<_amount
+              << ";deposit:" << deposit
+              << ";amount:"<< new_amount
+              << ";nb_deposits:" << _nbDeposits
+              << std::endl;   
+    _amount = new_amount;
+    _totalAmount += deposit;
+}
+
+bool Account::makeWithdrawal (int withdrawal )
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";p_amount:"<<_amount
+              << ";withdrawal:"; 
+    if (withdrawal > _amount)
+    {
+        std::cout << "refused" << std::endl;
+        return (false);
+    }
+    else
+    {
+        _nbWithdrawals++;
+        _totalNbWithdrawals++;
+        _amount -= withdrawal;
+        _totalAmount -= withdrawal;
+        std::cout << withdrawal
+                  << ";amount:"<< _amount
+                  << ";nb_withdrawals:" << _nbWithdrawals
+                  << std::endl; 
+        return (true);
+    }
+}
+
+int Account::checkAmount( void ) const
+{
+    return (_amount);
+}
+
+void Account::displayStatus( void ) const
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:"<< _amount
+              << ";deposits:" << _nbDeposits
+              << ";withdrawals:"<< _nbWithdrawals
+              << std::endl;
+}
+
+Account::~Account(void)
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";closed"
+              << std::endl;
+}
